80a1edc8eb679175de936c9dcd07cf52a6c73fed
tigeryam <1641341031@qq.com>
2022-04-02 17:15:41 +0800

    update ums_admin_sevice

--- mall-web/src/main/java/com/tiger/mall/config/MybatisConfig.java
+++ mall-web/src/main/java/com/tiger/mall/config/MybatisConfig.java
@@ -2,8 +2,13 @@ package com.tiger.mall.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * MyBatis相关配置
+ */
 @Configuration
-@MapperScan("com.tiger.mall.mapper")
+@EnableTransactionManagement
+@MapperScan({"com.tiger.mall.mapper","com.tiger.mall.dao"})
 public class MybatisConfig {
 }
